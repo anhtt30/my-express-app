db3795ece076c360b574fabf84192fae3fb1b62b
anhtt <anhttb01535@gmail.com>
2025-06-28 16:36:08 +0000

    commit

--- index.js
+++ index.js
@@ -1,17 +1,38 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const app = express();
-const PORT = 3000;
+const PORT = 4000;
 
 // Middleware to parse JSON
 app.use(express.json());
 
-// Sample route
-app.get('/', (req, res) => {
-    // response with json
-  res.json({ message: 'Hello from Express!' });
+// Connect to MongoDB
+mongoose.connect('mongodb://localhost:27017/express', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+})
+.then(() => console.log('MongoDB connected'))
+.catch(err => console.error('MongoDB connection error:', err));
+
+// Example schema
+const UserSchema = new mongoose.Schema({
+  name: String,
+  age: Number
 });
 
+const User = mongoose.model('User', UserSchema);
 
+// Sample route
+app.get('/api/hello', (req, res) => {
+    // response with json
+  res.json({ message: 'Hello from Express123!' });
+});
+
+// Sample route
+app.get('/api/users', async (req, res) => {
+  const users = await User.find();
+  res.json(users);
+});
 
 // Start the server
 app.listen(PORT, () => {
