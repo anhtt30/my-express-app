0af210badfee43e6f200c7c94d7503befa2dbc9a
anhtt <anhttb01535@gmail.com>
2025-07-29 15:29:21 +0000

    test 2

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ const User = mongoose.model('User', UserSchema);
 
 // Sample route
 app.get('/api/hello', (req, res) => {
-    // response with json
+    // response with json 123
   res.json({ message: 'Hello from Express123!' });
 });
 
